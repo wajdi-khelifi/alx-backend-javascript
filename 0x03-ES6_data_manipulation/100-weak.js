1288c072472ea5dc4b260d3496385a707c1735ae
wajdi-khelifi <khelifi.wajdi@hotmail.com>
2024-05-01 00:45:05 +0200

    map

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,20 @@
+const weakMap = new WeakMap();
+
+export { weakMap };
+
+function queryAPI(endpoint) {
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
+  }
+
+  const count = weakMap.get(endpoint) + 1;
+  weakMap.set(endpoint, count);
+
+  if (count >= 5) {
+    throw new Error('Endpoint load is high');
+  }
+
+  return count;
+}
+
+export default queryAPI;
