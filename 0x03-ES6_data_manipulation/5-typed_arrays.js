8d92cfd4177b9ea9833d434683de0783039bf6c2
wajdi-khelifi <khelifi.wajdi@hotmail.com>
2024-05-01 00:43:10 +0200

    project

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,14 @@
+function createInt8TypedArray(length, position, value) {
+  const buffer = new ArrayBuffer(length);
+  const view = new DataView(buffer);
+
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
+  }
+
+  view.setInt8(position, value);
+
+  return view;
+}
+
+export default createInt8TypedArray;
---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,15 @@
+// 9-groceries_list.js
+
+function groceriesList() {
+  const groceries = new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ]);
+
+  return groceries;
+}
+
+export default groceriesList;
